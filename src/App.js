57fbc3645540e74ffcc2aaa35922803dd9f51c4f
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-20 16:40:59 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Virtualized from './components/Virtualized';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const items = Array.from({ length: 2000000 }, (_, index) => ({
+      id: index,
+      content: `Item ${index}`,
+    }));
+  
+    return <Virtualized items={items} itemHeight={50} containerHeight={500} />;
+  
 }
 
 export default App;
---
+++ src/components/Virtualized.js
@@ -0,0 +1,43 @@
+
+import React, { useEffect, useState } from 'react'
+
+export default function Virtualized({ items, itemHeight, containerHeight }) {
+    const [scrollTop, setScrollTop] = useState(0);
+    const startIndex = Math.floor(scrollTop / itemHeight);
+    const endIndex = Math.min(
+        startIndex + Math.ceil(containerHeight / itemHeight),
+        items.length
+    );
+    const visibleItems = items.slice(startIndex, endIndex + 1);
+    useEffect(() => {
+        console.log(scrollTop)
+    })
+    const handleScroll = (e) => {
+        setScrollTop(e.currentTarget.scrollTop);
+    };
+    return (
+        <>
+            <h1 className='font-serif font-extrabold text-center my-6 text-4xl'><span>Virtualized</span> <span className='text-[#ff6868]'>List</span> </h1>
+            <div
+                style={{ height: `${containerHeight}px`, overflowY: "scroll" }}
+                onScroll={handleScroll}
+                className='bg-[#000000b9] text-white flex flex-col items-center shadow-xl rounded-lg mt-2 md:w-[70%] w-[90%] m-auto p-4'
+            >
+                <div style={{ height: `${items.length * itemHeight}px` }} >
+                    <div className='md:w-[456px] w-[200px]  ' 
+                        style={{
+                            position: "relative",
+                            height: `${visibleItems.length * itemHeight}px`,
+                            top: `${startIndex * itemHeight}px`,
+                        }}
+                    >
+                        {visibleItems.map((item) => (
+                            <div key={item.id} style={{ height: `${itemHeight}px` }}  data-aos="fade-right" className='bg-[#ff6767f3] text-white font-bold flex flex-col items-center shadow-xl rounded-lg mt-2 w-full m-auto p-4 transition-opacity duration-300 hover:opacity-75'>
+                                {item.content}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div></>
+    )
+}
